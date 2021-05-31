855a9ef0208e4cc7ae2e85c9f5380a2e13ad854b
anjubalaa <anjubalaa106@gmail.com>
2021-06-01 00:52:16 +0530

    petish

---
+++ resources/views/about.blade.php
@@ -0,0 +1,25 @@
+@extends('theme')
+@section('theme')
+
+
+    <div class="container" style="padding: 5%;" >
+        <div class="container" >
+        <h2 style="font-family: 'Trajan Pro';">PETISH</h2>
+       <br>
+          
+            <div class="container" style="background-color: rgba(221, 120, 213, 0.568); max-width: 100%;" >
+            <div class="row" style="padding: 5%;">
+                <h3 style="font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; color: rgb(255, 255, 255); text-align: center;">About Us</h3>
+            <p style="text-align: justify; font-family: Lato;">“Pet shops treat puppies, kittens, birds, hamsters, mice, rabbits, and other animals as if they were fashion accessories and sell them to anyone who plunks down a credit card. Selling animals denies homes to homeless and unwanted animals who await adoption in animal shelters.
+
+                Most animals sold in pet stores come from mass-breeding facilities called puppy mills, where they are denied socialization, exercise, and veterinary care. The puppies are typically taken from their mothers at an early age, packed into crates, and trucked or flown hundreds of miles to brokers and then to pet stores, often without adequate food, water, or ventilation. Some puppies don’t survive the grueling journey.
+                
+                Conditions at many pet stores are inadequate at best; at worst, they are outright abusive. Puppies are often kept in wire-bottomed cages; small animals such as mice, hamsters, gerbils, and rats are often crammed en masse into small, filthy, crowded cages; fish who were meant to swim freely in their ocean, lake, or river homes are reduced to circling the same few cubic inches of water in tanks that are often dirty and crowded; and exotic birds are typically kept in tiny cages, which prevents them from satisfying their natural needs to fly and socialize with others of their own species.
+                
+                Deprived of regular, loving human contact, puppies and kittens bought at pet stores are notoriously difficult to socialize and train. Compounded by the fact that, unlike good animal shelters, most pet stores don’t bother to screen potential animal adopters, this means that many animals who are purchased on a whim by unprepared people end up at animal shelters..;</p>
+                   
+          
+</div>
+            </div>
+
+    @endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,29 @@
+@extends('theme')
+@section('theme')
+
+    <div class="container" style="padding: 5%;" >
+        <div class="container" >
+        <h2 style="font-family: 'Trajan Pro';">PETISH</h2>      
+          
+        <div class="container" style="background-color:rgba(177, 81, 145, 0.466); max-width: 100%; padding: 2%; " ><center>
+          <div class="row" style=" max-width: 50%; padding: 5%; line-break: auto; background-color:rgba(111, 158, 204, 0.507); border-radius: 1em;"><center>
+            <p>
+              <h2 style="font-family: cursive; color:rgb(250, 249, 252)">Contact Us</h2>
+            <input type="text" class="form-control" style="max-width: 500px;" placeholder="Name"><br>
+            <input type="number" class="form-control" style="max-width: 500px;" placeholder="Phone Number"><br>
+            <input type="text" class="form-control" style="max-width: 500px;" placeholder="Email"><br>
+            <textarea type="text" class="form-control" style="max-width: 500px;" placeholder="Address"></textarea><br>
+            <input type="text" class="form-control" style="max-width: 500px;" placeholder="Feedback/Query"></p>
+            <button class="btn btn-outline-light  btn-sm" style="max-width: 100px;">Submit</button>
+            </div>
+          </div>
+          </center></center>
+
+
+
+
+
+
+
+<img src="/images/hr.jpg" height="500" width="100%" alt="image" style="opacity: 50%;">
+@endsection
